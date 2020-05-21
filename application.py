5bdc93775046a5206eb08a4199d0d2741176b679
jazzlikefruit <shewen_dave@hotmail.com>
2020-05-21 20:42:23 +0200

    formatted code

--- application.py
+++ application.py
@@ -1,3 +1,8 @@
+"""
+Book application
+A review website buit with Flask
+
+"""
 import os
 import requests
 from flask import Flask, session, render_template, request, url_for, redirect, jsonify
@@ -5,14 +10,14 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField
 from wtforms.validators import input_required, length
 from flask_bootstrap import Bootstrap
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'thiskeyissupposedtobesecret!'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -29,19 +34,21 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-# Pass the login information
-
 
 class loginForm(FlaskForm):
+    """ 
+    Form class to log user in 
+    """
     username = StringField('username', validators=[
                            input_required(), length(min=4, max=15)])
     password = PasswordField('password', validators=[
                              input_required(), length(min=8, max=25)])
 
-# Pass the sign up information
-
 
 class RegisterForm(FlaskForm):
+    """ 
+    Form class to register user
+    """
     username = StringField('username', validators=[
                            input_required(), length(min=4, max=15)])
     password = PasswordField('password', validators=[
@@ -51,25 +58,29 @@ class RegisterForm(FlaskForm):
 # index page
 @app.route("/")
 def index():
-
-    return render_template("index.html")
+    if "user" in session:
+        return redirect(url_for("homepage"))
+    else:
+        return render_template("index.html")
 
 # Log in page.
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    error = ''
+
+    # Take user input
     form = loginForm()
 
     # Check the user input.
     if form.validate_on_submit():
         user = db.execute("SELECT * FROM users WHERE username = :username",
                           {"username": form.username.data}).fetchone()
+
+        # If user found check the pasword and log user in
         if user:
             if check_password_hash(user.password, form.password.data):
                 session["user"] = form.username.data
                 return redirect(url_for('homepage'))
-        error = "invalid username/password. Try again."
-        return render_template("login.html", error=error, form=form)
+        return render_template("login.html", error="invalid username/password. Try again.", form=form)
 
     # Check if already logged in
     if "user" in session:
@@ -79,18 +90,23 @@ def login():
 # Registration page
 @app.route("/registration/", methods=["GET", "POST"])
 def registration():
-    error = ''
+
+    # Take user input
     form = RegisterForm()
+
+    # Check user input
     if form.validate_on_submit():
 
+        # Check if username in use
         if db.execute("SELECT * FROM users WHERE username =:username",
                       {"username": form.username.data}).fetchone():
-            error = 'Username unavailable'
-            return render_template("registration.html", form=form, error=error)
+            return render_template("registration.html", form=form, error='Username unavailable')
 
+        # Hash pasword
         hashed_password = generate_password_hash(
-            form.password.data, method='sha256')
+            form.password.data)
 
+        # Insert input in database
         db.execute(
             "INSERT INTO users (username, password) VALUES (:name, :pass)", {
                 "name": form.username.data,
@@ -105,23 +121,34 @@ def registration():
 # Home page
 @app.route("/homepage/")
 def homepage():
+    # Check if user is logged in
     if "user" in session:
+
+        # Query a list of 25 books
         books = db.execute("SELECT * FROM books LIMIT 25").fetchall()
+
+        # Return books
         return render_template("homepage.html", books=books)
+
     else:
         return redirect(url_for("login"))
 
 
-# search function
+# Search function
 @app.route("/search", methods=["POST"])
 def search():
     if "user" in session:
-        name = request.form.get("name").title()
-        results = db.execute(
+
+        # User search string
+        user_input = request.form.get("name").title()
+
+        # Check if book in database
+        if db.execute(
             "select * from books where autor LIKE :name or title LIKE :name or isbn LIKE :name",
-            {"name": "%" + name + "%"})
-        if results.rowcount == 0:
+                {"name": "%" + user_input + "%"}).rowcount == 0:
             return render_template("error.html", message="No results")
+
+        # Return results
         return render_template("search.html", results=results)
     else:
         return redirect(url_for("login"))
@@ -131,83 +158,92 @@ def search():
 @app.route("/search/<string:book_isbn>")
 def book(book_isbn):
     reviewed = False
-
     if "user" in session:
 
+        # Check if book exists
         book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {
             "isbn": book_isbn
         }).fetchone()
 
+        # Return error message if book not in database
         if book is None:
             return render_template("error.html", message="Book not found")
 
+        # Query average of reviews
         average = db.execute("SELECT AVG(round(rating)) from reviews where isbn = :isbn", {
-            "isbn": book_isbn}).fetchone()
+            "isbn": book_isbn})
+
+        # Query for book reviews
         reviews = db.execute(
             "SELECT * From reviews where isbn = :isbn", {"isbn": book_isbn}).fetchall()
 
-        review_made = db.execute("select * from reviews WHERE isbn = :isbn AND username = :username",
-                                 {"isbn": book_isbn, "username": session["user"]}).rowcount
+        # Check if book has reviews
+        if db.execute("select * from reviews WHERE isbn = :isbn AND username = :username",
+                      {"isbn": book_isbn, "username": session["user"]}).rowcount == 0:
 
-        if review_made == 0:
             reviewed = True
 
-        res = requests.get("https://www.goodreads.com/book/review_counts.json",
-                           params={"key": "JBL934ctLhK4oWIBJMKMEA", "isbns": book_isbn})
-        if res.status_code != 200:
+        # Query API fot book info
+        response = requests.get("https://www.goodreads.com/book/review_counts.json",
+                                params={"key": "JBL934ctLhK4oWIBJMKMEA", "isbns": book_isbn})
+
+        # Check response
+        if response.status_code != 200:
             raise Exception("ERROR: API request unsuccessful.")
-        data = res.json()
+        data = response.json()
+
+        # Gather needed data
         br_rating = data["books"][0]['ratings_count']
         br_average = data["books"][0]['average_rating']
         return render_template("book.html", book=book, reviews=reviews,
                                average_rating=average, reviewed=reviewed, br_rating=br_rating, br_average=br_average)
-
     else:
         return redirect(url_for("login"))
 
 # Review a book
 @app.route("/review/<string:book_isbn>", methods=["POST"])
 def review(book_isbn):
-    if request.method == "POST":
-        review_title = request.form["review-title"]
-        text = request.form["text"]
-        book_rating = request.form["book-rating"]
 
-        db.execute("INSERT INTO reviews(username, isbn, text, rating, review_title) VALUES (:username, :isbn, :text, :rating, :review_title)",
-                   {"username": session["user"],
-                    "isbn": book_isbn, "text": text,
-                    "rating": book_rating,
-                    "review_title": review_title})
-        db.commit()
-        return redirect(url_for('book', book_isbn=book_isbn))
+    # Insert user review in database
+    db.execute("INSERT INTO reviews(username, isbn, text, rating, review_title) VALUES (:username, :isbn, :text, :rating, :review_title)",
+               {"username": session["user"],
+                "isbn": book_isbn, "text": request.form["text"],
+                "rating": request.form["book-rating"],
+                "review_title": request.form["review-title"]})
+    db.commit()
+
+    return redirect(url_for('book', book_isbn=book_isbn))
 
 # Logout function
 @app.route("/logout")
 def logout():
     session.pop("user", None)
-    return redirect(url_for("login"))
+    return redirect(url_for("index"))
 
 # Return details about a book.
 @app.route("/api/<string:book_isbn>")
 def books_api(book_isbn):
 
     # Make sure book exists.
-    book = db.execute("SELECT * from books WHERE isbn= :isbn",
-                      {"isbn": book_isbn}).fetchone()
-    if not book:
+    if not db.execute("SELECT * from books WHERE isbn= :isbn",
+                      {"isbn": book_isbn}).fetchone():
         return jsonify({"error": "Invalid isbn"}), 422
 
+    # Query for book reviews
     review_info = db.execute(
         "SELECT * from reviews WHERE isbn=:isbn", {"isbn": book_isbn}).rowcount
 
+    # Query for book average
     average_info = db.execute("SELECT AVG(round(rating)) FROM reviews WHERE isbn = :isbn", {
         "isbn": book_isbn}).fetchone()
 
+    # Set average as 0 if book not reviewed
     if average_info[0] == None:
         average_info = 0
     else:
-        average_info = averageinfo[0]
+        average_info = average_info[0]
 
+    # Return info in json format
     return jsonify({
         "title": book['title'],
         "author": book['autor'],
--- import.py
+++ import.py
@@ -13,23 +13,18 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # Link the Flask app with the database (no Flask app is actually being run yet).
 db.init_app(app)
 
-
 def main():
-    db.create_all()
-    
+
     file = open("books.csv")
     reader = csv.reader(file)
     next(reader)
     for isbn, title, autor, publication_year in reader:  # loop gives each column a name
-        book = Books(isbn=isbn, title=title, autor=autor,
-                     publication_year=publication_year)
-        db.session.add(book)
+        db.execute(
+            "INSERT INTO books (isbn, title, autor, publication_year) VALUES(:isbn, :title, :autor, :publication_year)")
+
         print(
             f"Added book {isbn} titled {title} writen by {autor} in the year {publication_year}."
         )
-        db.session.flush()
-    db.session.commit()
-
 
 if __name__ == "__main__":
     # Allows for command line interaction with Flask application
